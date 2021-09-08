a20462ab1cddea551b69ee3c2a01f4fd09652cd4
Nerixyz <nero.9@hotmail.de>
2021-09-08 12:15:21 +0200

    test: multiple waiters

--- tests/index.ts
+++ tests/index.ts
@@ -1,5 +1,5 @@
 import { Semaphore } from '../src';
-import { notWaiting } from './utils';
+import { notWaiting, oneshot, orderedThen, waiting } from './utils';
 import * as timers from 'timers/promises';
 
 describe('Semaphore', function () {
@@ -56,6 +56,34 @@ describe('Semaphore', function () {
       });
       await expect(promise).rejects.toBeDefined();
       expect(semaphore.availablePermits).toBe(1);
+      notWaiting(semaphore);
+    });
+  });
+
+  describe('multiple waiters', function () {
+    it('should handle multiple waiters correctly', async function () {
+      const semaphore = new Semaphore(1);
+
+      const ordered = orderedThen();
+
+      const waiter1 = oneshot();
+      const waiter2 = oneshot();
+      const waiter3 = oneshot();
+
+      const acq1 = semaphore.acquireWith(() => waiter1.promise).then(ordered.expect(0));
+      const acq2 = semaphore.acquireWith(() => waiter2.promise).then(ordered.expect(1));
+      const acq3 = semaphore.acquireWith(() => waiter3.promise).then(ordered.expect(2));
+
+      waiting(semaphore);
+
+      waiter1.resolve();
+      await acq1;
+      waiting(semaphore);
+
+      waiter2.resolve();
+      await acq2;
+      waiter3.resolve();
+      await acq3;
       notWaiting(semaphore);
     });
   });
--- tests/utils.ts
+++ tests/utils.ts
@@ -1,5 +1,37 @@
 import { Semaphore } from '../src';
 
+export function waiting(sem: Semaphore) {
+  expect(sem.isWaiting).toBe(true);
+}
+
 export function notWaiting(sem: Semaphore) {
   expect(sem.isWaiting).toBe(false);
+}
+
+export function oneshot<T = void>(): { promise: Promise<T>, resolve: (value: T) => void, reject: (error: Error) => void } {
+  let resolve = null;
+  let reject = null;
+  const promise = new Promise<T>((res, rej) => {
+    resolve = res;
+    reject = rej;
+  });
+
+  if(!resolve || !reject) {
+    throw new Error("No resolve or reject");
+  }
+
+  return {promise, resolve, reject};
+}
+
+export function orderedThen() {
+  let stage = 0;
+
+  return {
+    expect: (expectedStage: number) => () => {
+      if (stage != expectedStage) {
+        throw new Error(`Expected stage ${expectedStage} but was actually in stage ${stage}`);
+      }
+      stage++;
+    }
+  }
 }
