788c8ec2a3bdbb51abb824995e37514270f0e934
Nerixyz <nero.9@hotmail.de>
2021-09-10 22:36:16 +0200

    feat: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'arrow-body-style': 'off',
+    'prefer-arrow-callback': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+};
