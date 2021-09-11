dedc348ce7cfdc51f5c0002dd7d027444cb27baa
Nerixyz <nero.9@hotmail.de>
2021-09-11 17:34:41 +0200

    test: rename tests to .test.ts

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-undef */
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testRegex: "tests/.*\\.[jt]s$",
+  testRegex: 'tests/.*\\.test\\.[jt]s$',
+  coveragePathIgnorePatterns: ['test'],
 };
